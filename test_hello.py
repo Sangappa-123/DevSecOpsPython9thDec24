29fea5d96c956aab8efd3e89880bcf796ed40ad4
Sangappa-123 <birajsd@gmail.com>
2024-12-10 11:40:31 +0530

    Create test_hello.py

---
+++ test_hello.py
@@ -0,0 +1,10 @@
+# test_hello.py
+import unittest
+from hello import say_hello
+
+class TestHello(unittest.TestCase):
+    def test_say_hello(self):
+        self.assertEqual(say_hello(), "Hello, GitHub Actions!")
+
+if __name__ == "__main__":
+    unittest.main()
